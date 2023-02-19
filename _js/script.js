bc2acf0a84ceb76e6cc9fe7092c42dbbe9001e3c
JuanSoulAxel <barruan.mm@gmail.com>
2023-02-19 11:32:43 -0300

    Criando a parte inicial do site: menu, novidades, categorias e carrossel de imagens

---
+++ _js/script.js
@@ -0,0 +1,17 @@
+//CARROSSEL
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img"); //entrando no id img e pegando as tags img
+
+let idx = 0;
+
+function carrossel() {
+    idx++;
+
+    if(idx > img.length - 1) {//se idx for maior que o tamanho da img - 1
+        idx = 0; //quando passar a última imagem, ele voltará para a imagem inicial
+    } 
+
+    imgs.style.transform = `translateX(${-idx * 100}%)`;
+}
+
+setInterval(carrossel, 3000);
